8d052001f71bd283d27e76aafb3b8e8c9c16c5c3
Pawandadra <pawankumarpk3610@gmail.com>
2024-07-20 10:03:29 +0530

    sucsessfully deployed on vercel

--- api/server.js
+++ api/server.js
@@ -10,18 +10,4 @@ app.get('/:countryName', (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'details.html'));
 });
 
-// Default route for undefined paths
-app.get('*', (req, res) => {
-    res.status(404).send('Page not found');
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
-
-// Export the Express app as a serverless function
-module.exports = (req, res) => {
-    app(req, res);
-};
+module.exports = app;
